c8b9e59c813c51f0207ed9a1f0d1a2b9d58a5e0b
Lise Martinsen <lise.martinsen@yh.nackademin.se>
2024-09-10 08:29:34 +0200

    initial commit

---
+++ src/main/java/org/example/cv_devops/CvDevopsApplication.java
@@ -0,0 +1,13 @@
+package org.example.cv_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvDevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cv_devops/CvDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cv_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
